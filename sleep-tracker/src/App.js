41654dec6f691defdbd3e7b1f1695b237be6766a
Brian Russell <bruss@brians-mbp.lan>
2020-04-27 11:51:17 -0400

    Added Routing to index.js and App.js, also added a Home.js component

--- sleep-tracker/src/App.js
+++ sleep-tracker/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import Home from './components/Home.js';
+import Login from './components/Login.js';
+import Signup from './components/Signup.js';
+import Dashboard from './components/Dashboard.js';
+import OptimalSleep from './components/OptimalSleep';
+import WeeklyView from './components/WeeklyView.js';
+import EntryView from './components/EntryView.js';
 
 /*
 TODO: Import and route components:
-    "/"           --> Home.js (exact!)
-    "/login"      --> Login.js
-    "/signup"    --> Signup.js
-    "/dashboard"  --> Dashboard.js
-    "/optimal-sleep" --> OptimalSleep.js
-    "/weekly-view:start-date" --> WeeklyView.js
-    "/entry"      --> EntryView.js (exact!)
-    "/entry:date" --> EntryView.js 
+  √  "/"           --> Home.js (exact!)
+  √  "/login"      --> Login.js
+  √  "/signup"    --> Signup.js
+  √  "/dashboard"  --> Dashboard.js
+  √  "/optimal-sleep" --> OptimalSleep.js
+  √  "/weekly-view:start-date" --> WeeklyView.js
+  √  "/entry"      --> EntryView.js (exact!)
+  √  "/entry:date" --> EntryView.js 
 
     ***Components are already set up so they can be imported.
 
@@ -19,7 +27,43 @@ TODO: Import and route components:
 
 function App() {
   return (
-    <div>Root App Component</div>
+    <div>
+      <Switch>
+
+        <Route path='/login'>
+          <Login />
+        </Route>
+
+        <Route path='/signup'>
+          <Signup />
+        </Route>
+
+        <Route path='/dashboard'>
+          <Dashboard />
+        </Route>
+
+        <Route path='/optimal-sleep'>
+          <OptimalSleep />
+        </Route>
+
+        <Route path='/weekly-view:start-date'>
+          <WeeklyView />
+        </Route>
+
+        <Route path ='/entry:date'>
+          <EntryView />
+        </Route>
+
+        <Route path='/entry'>
+          <EntryView />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+
+      </Switch>
+    </div>
   );
 }
 
