8d114579e9a81e2f241cf5f52956d1b18dfeb683
Brian Russell <bruss@brians-mbp.lan>
2020-04-27 13:45:40 -0400

    Created basic layout of Login and Signup components

--- sleep-tracker/src/components/Login.js
+++ sleep-tracker/src/components/Login.js
@@ -1,5 +1,40 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Login = _ => ( <h1>This is the login page</h1> )
+const Login = _ => ( 
+
+<div>
+    <h1>Login</h1>
+
+    <form>
+        <label>Username
+            <br />
+            <input
+                name='username'
+                type='text'
+            ></input>
+        </label>
+
+        <br /><br />
+
+        <label>Password
+            <br />
+            <input
+                name='password'
+                type='password'
+            ></input>
+        </label>
+
+        <br /><br />
+        <button>Log In</button>
+
+        <br />
+        
+        <Link to='/signup'>
+            <button>Sign Up</button>
+        </Link>
+    </form>
+</div>
+)
 
 export default Login
--- sleep-tracker/src/components/Signup.js
+++ sleep-tracker/src/components/Signup.js
@@ -1,4 +1,70 @@
 import React from 'react'
 
-const Signup = _ => ( <h1>This is the signup page</h1> )
+const Signup = _ => ( 
+<div>
+    <h1>Signup</h1> 
+
+    <form>
+        <label>First Name
+            <br />
+            <input
+                name='first-name'
+                type='text'
+            ></input>
+        </label>
+
+        <br /><br />
+
+        <label>Last Name
+            <br />
+            <input
+                name='last-name'
+                type='text'
+            ></input>
+        </label>
+
+        <br /><br />
+
+        <label>Age
+            <br />
+            <input></input>
+        </label>
+
+        <br /><br />
+
+        <label>Email Address
+            <br />
+            <input
+                name='email'
+                type='email'
+            ></input>
+        </label>
+
+        <br /><br />
+
+        <label>Select Your Username
+            <br />
+            <input
+                name='username'
+                type='text'
+            ></input>
+        </label>
+
+        <br /><br />
+
+        <label>Create Password
+            <br />
+            <input
+                name='password'
+                type='password'
+            ></input>
+        </label>
+
+        <br /><br />
+
+        <button>Submit</button>
+    </form>
+
+</div>
+)
 export default Signup
