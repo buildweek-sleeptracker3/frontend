57940ee29057e68edc0b9868c032929fc2f917b8
Brian Russell <bruss@brians-mbp.lan>
2020-04-27 20:08:19 -0400

    Updated Login and Signup, and added some styling

--- sleep-tracker/src/components/Login.js
+++ sleep-tracker/src/components/Login.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+const LoginStyle =styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 375px;
+    padding: 5px;
+
+    form {
+
+            input{
+                width: 100%;
+                height: 30px;
+                border-radius: 5px 5px 0 0; 
+                box-sizing: border-box;
+
+            }
+
+            button{
+                width: 100%;
+                height: 30px;
+                border-radius: 10px;
+            }
+    }
+`
 
 const Login = _ => ( 
 
-<div>
+<LoginStyle>
     <h1>Login</h1>
 
     <form>
-        <label>Username
-            <br />
+        <label>Username <br />
             <input
                 name='username'
                 type='text'
@@ -17,8 +43,7 @@ const Login = _ => (
 
         <br /><br />
 
-        <label>Password
-            <br />
+        <label>Password   <br />
             <input
                 name='password'
                 type='password'
@@ -28,13 +53,13 @@ const Login = _ => (
         <br /><br />
         <button>Log In</button>
 
-        <br />
-        
+        <br />< br/>
+
         <Link to='/signup'>
             <button>Sign Up</button>
         </Link>
     </form>
-</div>
+</LoginStyle>
 )
 
 export default Login
--- sleep-tracker/src/components/Signup.js
+++ sleep-tracker/src/components/Signup.js
@@ -1,70 +1,135 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios'
 
-const Signup = _ => ( 
-<div>
-    <h1>Signup</h1> 
 
-    <form>
-        <label>First Name
-            <br />
-            <input
-                name='first-name'
-                type='text'
-            ></input>
-        </label>
+const SignupStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 375px;
+    padding: 5px;
 
-        <br /><br />
+    form {
 
-        <label>Last Name
-            <br />
-            <input
-                name='last-name'
-                type='text'
-            ></input>
-        </label>
+            input{
+                width: 100%;
+                height: 30px;
+                border-radius: 5px 5px 0 0; 
+                box-sizing: border-box;
 
-        <br /><br />
+            }
 
-        <label>Age
-            <br />
-            <input></input>
-        </label>
+            button{
+                width: 100%;
+                height: 30px;
+                border-radius: 10px;
+            }
+    }
+`
 
-        <br /><br />
+const blankForm = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: '',
+}
 
-        <label>Email Address
-            <br />
-            <input
-                name='email'
-                type='email'
-            ></input>
-        </label>
 
-        <br /><br />
+const postUser = user => {
+    axios.post('', user)
+    .then(res =>{
+      console.log(res)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+}
 
-        <label>Select Your Username
-            <br />
-            <input
-                name='username'
-                type='text'
-            ></input>
-        </label>
 
-        <br /><br />
+const Signup = _ => { 
 
-        <label>Create Password
-            <br />
-            <input
-                name='password'
-                type='password'
-            ></input>
-        </label>
+    const [formValues, setFormValues] = useState(blankForm)
 
-        <br /><br />
 
-        <button>Submit</button>
-    </form>
+    const onSubmit = evt => {
+        evt.preventDefault()
+    
+        const newUser = {
+            firstName: formValues.firstName,
+            lastName: formValues.lastName,
+            email: formValues.email,
+            username: formValues.username,
+            password: formValues.password,
+        }
+        
+        postUser(newUser)
+        setFormValues(blankForm)
+    }
 
-</div>
-)
+    return(
+        <SignupStyle>
+            <h1>Sign Up!</h1> 
+
+            <form onSubmit={onSubmit} >
+                <label>First Name <br />
+                    <input
+                        name='firstName'
+                        type='text'
+                    ></input>
+                </label>
+
+                <br /><br />
+
+                <label>Last Name <br />
+                    <input
+                        name='lastName'
+                        type='text'
+                    ></input>
+                </label>
+
+                <br /><br />
+
+                {/* <label>Age <br />
+                    <input></input>
+                </label>
+
+                <br /><br /> */}
+
+                <label>Email Address <br />
+                    <input
+                        name='email'
+                        type='email'
+                    ></input>
+                </label>
+
+                <br /><br />
+
+                <label>Select Your Username <br />
+                    <input
+                        name='username'
+                        type='text'
+                    ></input>
+                </label>
+
+                <br /><br />
+
+                <label>Create Password <br />
+                    <input
+                        name='password'
+                        type='password'
+                    ></input>
+                </label>
+
+                <br /><br />
+
+                <button>Submit</button>
+            </form>
+
+        </SignupStyle>
+    )
+}
+
 export default Signup
