cc2d4e6551e4f1b187d3dc9ea44c58bb8ffa13fe
Victoria  Topham <topham.victoria@gmail.com>
2020-04-30 11:25:49 -0700

    cleaned up the dashboard

--- sleep-tracker/src/components/Dashboard.js
+++ sleep-tracker/src/components/Dashboard.js
@@ -6,6 +6,16 @@ import { fetchSleepData } from '../actions/index'
 import { axiosWithAuth } from '../utils/axiosWithAuth'
 import { formatData } from '../utils/formatData'
 
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    margin: 0 2%;
+    .graph {
+        display: flex;
+        justify-content: center;
+        background: #a9a9a9;
+    }
+`
 
 const mapStateToProps = state => {
     return {
@@ -23,12 +33,17 @@ const Dashboard = props => {
     const [sleepData, setSleepData] = useState([])
 
     //update state using a get request, done by brian
-    axiosWithAuth()
+
+    useEffect(_ => {
+        axiosWithAuth()
         .get('/api/users/sleep')
         .then(res => {
            setSleepData(res.data)
         }) 
         .catch(err => console.log(err))
+
+    },[])
+    
     
 
     //filter the state before feeding it to aggregated mood to format
@@ -56,23 +71,43 @@ const Dashboard = props => {
         
     }
 
+    //themes for the graph
 
+    const axisStyle = {
+        stroke: "#e3e3e3",
+        strokeWidth: 2,
+        // label: PropTypes.shape({
+        //   bottom: PropTypes.object,
+        //   top: PropTypes.object,
+        // })
+    }
+
+    const tickStyle = { //doesn't do anything
+            stroke: "red",
+            tickLength: 1,
+            // label: PropTypes.shape({
+            //   bottom: PropTypes.object,
+            //   top: PropTypes.object,
+            // }
+        }
 
     //display a graph of the sleep data vs. mood
     if (!aggregatedMood) {return null}
 
     return ( 
-        <>
-            <h1>Your Sleep Mood</h1>
-            <h2> You're at your best when you get {maxKey} hours of sleep.</h2>
+        <StyledDiv>
+            <h1>Your Sleep Stats</h1>
+            <p> You're at your best when you get {maxKey} hours of sleep.</p>
 
-            <HomeButton />
-            <Histogram width = "700" height = "500" binType = "categorical">
-                <BarSeries binnedData = {aggregatedMood.binned}/>
-                <XAxis label = "Hours of sleep"/>
-                <YAxis label = "Mood"/>
-            </Histogram>
-        </> 
+            
+            <div className = "graph">
+                <Histogram className = "bar-chart" width = "700" height = "500" binType = "categorical">
+                    <BarSeries className = "bar-series" binnedData = {aggregatedMood.binned}/>
+                    <XAxis axisStyles = {axisStyle} className = "axis" label = "Hours of sleep"/>
+                    <YAxis axisStyles = {axisStyle} className = "axis" label = "Mood"/>
+                </Histogram>
+            </div>
+        </StyledDiv> 
     )
 }
 
