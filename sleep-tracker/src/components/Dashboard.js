0a7e5c51aa4c4db5989e7bf0718322ec55f9a153
Victoria  Topham <topham.victoria@gmail.com>
2020-04-30 08:46:26 -0700

    set up some fun stuff for brian to do in dashboard.js

--- sleep-tracker/src/components/Dashboard.js
+++ sleep-tracker/src/components/Dashboard.js
@@ -6,23 +6,48 @@ import { fetchSleepData } from '../actions/index'
 
 import { formatData } from '../utils/formatData'
 
+
 const mapStateToProps = state => {
     return {
-        moodData: state.sleepMood,
-        rawData: state.data
+        // moodData: state.sleepMood,
+        rawData: state.data, //<----comment me out when done BRIAN TODO
+        userId: state.userId //<--- use props.id to access the user userId
     }
 }
+
+
+//BRIAN TODO - MAKE A GET REQUEST HERE AND SET UP STATE
 
 //This renders the sleep/mood calculations
 const Dashboard = props => {
 
-    /////////
+    /////////BRIAN TODO - YOU CAN COMMENT OUT THIS USEEFFECT WHEN YOU'RE DONE
     useEffect(_ => {
         props.fetchSleepData()
 
     },[])
+    ///////END COMMENTING
 
-    const aggregatedMood = formatData(props.rawData)
+    //BRIAN TODO #1 - SET UP STATE USING USESTATE HOOK
+
+    //BRIAN TODO #2 - UPDATE STATE USING A GET REQUEST
+
+//     axiosWithAuth()
+//         .get('/api/users/sleep')
+//         .then(res => {
+//             <--- UPDATE LOCAL STATE WITH RES AFTER FILTERING IT BY USERID
+//         .catch(err => console.log(err))
+//      }
+
+    //how to filter
+    //tempArray =  YOUR RESPONSE ARRAY.map(item => {
+    //     if(item.userId.toString() === props.userId) {
+    //         return item
+    //     }
+        
+    //     })
+
+    const aggregatedMood = formatData(props.rawData) //replace props.raw data with your state
     
 
     
@@ -30,13 +55,13 @@ const Dashboard = props => {
     /////////
     
     //we have to update the shape of the data for it to work with the graphing library
-    const binnedData = Object.keys(props.moodData).map(key => {
-        return {
-            id: key,
-            bin: key,
-            count: props.moodData[key]
-        }
-    })
+    // const binnedData = Object.keys(props.moodData).map(key => {
+    //     return {
+    //         id: key,
+    //         bin: key,
+    //         count: props.moodData[key]
+    //     }
+    // })
 
 
     let maxKey = ""
